6fdc2010127f3addc6b41b9ba96f95c82f3cbcf3
marekkawalski <marek.kawalski@gmail.com>
2024-08-11 13:27:33 +0200

    Minor changes

--- src/main/java/com/marek_kawalski/clinic_system/seeder/Constants.java
+++ src/main/java/com/marek_kawalski/clinic_system/seeder/Constants.java
@@ -5,8 +5,8 @@ public class Constants {
     }
 
     public static final Integer NUMBER_OF_ADMIN_USERS = 10;
-    public static final Integer NUMBER_OF_DOCTOR_USERS = 100;
-    public static final Integer NUMBER_OF_PATIENT_USERS = 100;
+    public static final Integer NUMBER_OF_DOCTOR_USERS = 150;
+    public static final Integer NUMBER_OF_PATIENT_USERS = 150;
     public static final Integer NUMBER_OF_REGISTRAR_USERS = 10;
     public static Integer NUMBER_OF_APPOINTMENTS = 100;
 }
